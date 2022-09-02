be0a253364a98953ff04b21ba51a20c50a5a202b
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-09-03 06:48:07 +0900

    simple client request > server response
    get 함수 이용

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const express1= express();//express 객체
 
 const server = http.createServer(express1);//express로부터 server 생성
 
+express1.get('/', function(request,response){
+console.log("user entered!"),
+response.send("client: hello express server"), console.log("hi")});
+
 server.listen(8080, function(){
     console.log('server is now running!, listner 실행.')});
+
