5f69a463310e8ae7ea325d87f764cde47a5e988f
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-09-03 05:18:42 +0900

    서버와 리스너 바인딩, 변수 이름 중복 오류 수정
    -사용 메소드 listen(port#, listner)
    -포트 번호 8080 지정
    포트번호란?? localhost:8080 로 웹에서 접속가능하도록.
    -함수내용을 안에다 한번에 정의가능.
    -기능: 1. 지정 포트번호로 서버를 실행시, > 2.listen함수에 등록한 리스너가 호출
    
    -실행되는 것 확인하는 법.
    터미널(맥) node index.js >> 리스너 함수 콘솔 내용이 출력되어야.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const express = require('express');
-const http = require('http');
-const http = require('socket.io');
+const express = require('express');//서버
+const http = require('http');//서버
+const socket = require('socket.io');//실시간 서버-클라이언트 채팅
+
+const express1= express();//express 객체
+
+const server = http.createServer(express1);//express로부터 server 생성
+
+server.listen(8080, function(){
+    console.log('server is now running!, listner 실행.')});
